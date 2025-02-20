c0fd276d680cc2b15e25e8f324a9b5f43314517b
eakwofie <amankrahkwofie354@gmail.com>
2025-02-20 09:42:23 -0500

    configured analytics tracting from vercel

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 
 // Replace the Inter font with Playfair Display and Source Sans Pro
 import { Playfair_Display, Source_Sans_3 } from "next/font/google";
@@ -43,6 +44,7 @@ export default function RootLayout({
             <Footer />
           </div>
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
